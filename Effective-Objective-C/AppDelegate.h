b8c00642446627962f569b08a8032770e6ebdba6
happyer-lwl <runlwl@163.com>
2016-12-02 09:55:45 +0800

    Initial Commit

---
+++ Effective-Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Effective-Objective-C
+//
+//  Created by WyzcWin on 16/12/2.
+//  Copyright © 2016年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Effective-Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Effective-Objective-C
+//
+//  Created by WyzcWin on 16/12/2.
+//  Copyright © 2016年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
